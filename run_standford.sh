ee74738ad231508472d89887dff30fd21ad8f6bb
Tung Nguyen <tung.nguyen@titanshark.com>
2017-12-07 08:30:52 +0100

    Run script with more executors and memory

--- run_standford.sh
+++ run_standford.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-ATT=${1:-"WARC-Record-ID"}
+TT=${1:-"WARC-Record-ID"}
 INFILE=${2:- "hdfs:///user/bbkruit/CC-MAIN-20160924173739-00000-ip-10-143-35-109.ec2.internal.warc.gz"}
 
 DATE=`date '+%Y_%m_%d_%H_%M_%S'`
@@ -8,5 +7,7 @@ DRIVER_PROGRAM=entity_linking_standford.py
 
 
 echo "Start driver program =============> Output: $DATE"
-spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./NLTK/nltk_env/bin/python --conf spark.yarn.appMasterEnv.NLTK_DATA=./ --master yarn-cluster --driver-memory 800mb --executor-memory 800mb --files english.all.3class.distsim.crf.ser.gz,english-left3words-distsim.tagger,stanford-ner.jar,stanford-postagger-3.8.0.jar --py-files dependencies02.zip  --archives nltk_env.zip#NLTK,tokenizers.zip#tokenizers,taggers.zip#taggers,chunkers.zip#chunkers --conf spark.yarn.maxAppAttempts=1 $DRIVER_PROGRAM $ATT $INFILE $OUTFILE
+
+spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./NLTK/nltk_env/bin/python --conf spark.yarn.appMasterEnv.NLTK_DATA=./ --master yarn-cluster --num-executors 16 --executor-cores 4  --executor-memory 4G  --files english.all.3class.distsim.crf.ser.gz,english-left3words-distsim.tagger,stanford-ner.jar,stanford-postagger-3.8.0.jar --py-files dependencies02.zip  --archives nltk_env.zip#NLTK,tokenizers.zip#tokenizers,taggers.zip#taggers,chunkers.zip#chunkers --conf spark.yarn.maxAppAttempts=1 $DRIVER_PROGRAM $ATT $INFILE $OUTFILE
+
 echo "End driver program ================"
