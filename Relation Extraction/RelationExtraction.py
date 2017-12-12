fd42092bdad24ee3ce8346a5e28074ccd74c9011
Felipe Souza <Felipe.Santos.Batista.de.Souza@ing.nl>
2017-12-12 21:21:34 +0100

    Relation Extraction - POC - More types of relations // Analysis - Most Common verbs x Top1000 words from WARC sample

--- Relation Extraction/RelationExtraction.py
+++ Relation Extraction/RelationExtraction.py
@@ -14,7 +14,11 @@ os.environ["PYTHONPATH"] = "${PYTHONPATH}:/home/wdps1705/.local/lib/python2.6/si
 
 sample = 'Bob works at Microsoft in New York.'
 sample2 = ' John works at Microsoft which is located in San Francisco.'
-sample3 = ' Eduard is a developer at Microsoft in London'
+sample3 = ' Eduard is a developer at Microsoft in London.'
+sample4 = ' John teaches at Xerox located in New York in United States.'
+sample5 = ' The president Obama says to Hillary Clinton to try the elections for president of United States.'
+sample6 = ' John goes to Brazil for vacations every year but he lives in Seatle.'
+
 
 def extract_relations(text, typeRelations):
 
@@ -47,11 +51,33 @@ def extract_relations(text, typeRelations):
 
                         # Extracts the relation.
                         rels = extract_rels('PER', 'ORG', chunked_sentence, corpus='ace', pattern=REL_RE, window=10)
+                        rels2 = extract_rels('ORG', 'GPE', chunked_sentence, corpus='ace', pattern=REL_RE, window=10)
+
+                        rels3 = extract_rels('PER', 'PER', chunked_sentence, corpus='ace', pattern=REL_RE, window=10)
+                        rels4 = extract_rels('PER', 'GPE', chunked_sentence, corpus='ace', pattern=REL_RE, window=10)
 
                         for rel in rels:
                                 print(rtuple(rel))
                                 # Adds the relation to the final list of relations.
                                 relations.append(rel)
+
+                        for rel in rels2:
+                                print(rtuple(rel))
+                                # Adds the relation to the final list of relations.
+                                relations.append(rel)
+
+
+                        for rel in rels3:
+                                print(rtuple(rel))
+                                # Adds the relation to the final list of relations.
+                                relations.append(rel)
+
+                        for rel in rels4:
+                                print(rtuple(rel))
+                                # Adds the relation to the final list of relations.
+                                relations.append(rel)
+
+
 
             # Prints the extracted relations
             for relation in relations:
@@ -59,11 +85,13 @@ def extract_relations(text, typeRelations):
 
 if __name__ == '__main__':
 
-        text= sample + sample2+sample3
+        text= sample + sample2+sample3+sample4+sample5+sample6
 
         typeRelations = []
         typeRelations.append('works')
         typeRelations.append('is.*at')
+        typeRelations.append('located')
+        typeRelations.append('say')
+        typeRelations.append('go')
 
         extract_relations(text,typeRelations)
-
